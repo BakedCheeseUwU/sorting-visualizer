b68360b0ecfaf4c5b836b165b1b32f63220795d5
Aditya <aditya.sasmal.dev@gmail.com>
2023-11-01 01:07:47 +0530

    Adds Buttons for sorting

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -23,20 +23,42 @@ componentDidMount() {
     this.setState({array});
   }
 
+  mergeSort() {
+  }
+
+  quickSort() {
+  }
+
+  heapSort() {
+  }
+
+  bubbleSort() {
+  }
+
 render() {
     const {array} = this.state;
 
     return (
       <div className="array-container">
         {array.map((value, idx) => (
-          <div
-            className="array-bar"
-            key={idx}
-            style={{
-                height: `${value}px`,
-              }}
-            ></div>
-        ))}
+            <div
+                className="array-bar"
+                key={idx}
+                style={{
+                    height: `${value}px`,
+                }}
+                ></div>
+            ))}
+            <button onClick={() => this.resetArray()}>Generate New Array</button>
+            <button onClick={() => this.mergeSort()}>Merge Sort</button>
+            <button onClick={() => this.quickSort()}>Quick Sort</button>
+            <button onClick={() => this.heapSort()}>Heap Sort</button>
+            <button onClick={() => this.bubbleSort()}>Bubble Sort</button>
+            <button onClick={() => this.testSortingAlgorithms()}>
+            Test Sorting Algorithms (BROKEN)
+            </button>
+
+
       </div>
     );
   }
